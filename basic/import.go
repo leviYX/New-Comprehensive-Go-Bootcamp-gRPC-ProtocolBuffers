2439317fbc103fe44aafcd32c35d49cfff6685f8
liuwenqiang <wenqiang.liu@wenge.com>
2025-06-11 11:09:11 +0800

    关于包的导入import

---
+++ basic/import.go
@@ -0,0 +1,23 @@
+package basic
+
+import (
+	"fmt"
+	myhttp "net/http"
+)
+
+const (
+	baiduURL  = "https://www.baidu.com"
+	googleURL = "https://www.google.com"
+)
+
+func main() {
+	resp, err := myhttp.Get(baiduURL)
+	if err != nil {
+		fmt.Println("http get error:", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+}
